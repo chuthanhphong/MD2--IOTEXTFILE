6883164d7d184117767aa9dc1d563e3af0d66a36
chuthanhphong <81786056+chuthanhphong@users.noreply.github.com>
2021-05-31 11:04:07 +0700

    add

--- untitled/src/bt5/main.java
+++ untitled/src/bt5/main.java
@@ -11,50 +11,90 @@ public class main {
     public static void main(String[] args) {
         File file = new File("country.csv");
         try {
-            if(!file.exists()){
+            if (!file.exists()) {
                 file.createNewFile();
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("file đã tồn tại");
         }
+//        BufferedReader br = null;
+//        try {
+//            String line ;
+//            br = new BufferedReader(new FileReader("country.csv"));
+//            while ((line=br.readLine())!=null){
+//                    prinkCountry(parseCsvLine(line));
+//            }
+//            System.out.println("done");
+//        } catch (IOException e) {
+//            System.out.println("Wrong");
+//        }
+//        finally {
+//            try {
+//                if(br!=null){
+//                    br.close();
+//                }
+//            }catch (IOException E){
+//                E.printStackTrace();
+//            }
+//        }
+//        System.out.println(file.getAbsolutePath());
+//    }
+//    public  static List<String> parseCsvLine(String csvLine){
+//        List<String> result = new ArrayList<>();
+//        if(csvLine!=null){
+//            String[] splitData = csvLine.split(",");
+//            for(int i =0;i<splitData.length;i++){
+//                result.add(splitData[i]);
+//            }
+//        }
+//        return result;
+//    }
+//    public  static  void prinkCountry(List<String > country){
+//        System.out.println( "Country [id= "
+//                + country.get(0)
+//                + ", code= " + country.get(1)
+//                + " , name=" + country.get(2)
+//                + "]");
+//    }
         BufferedReader br = null;
         try {
-            String line ;
+            String line;
             br = new BufferedReader(new FileReader("country.csv"));
-            while ((line=br.readLine())!=null){
-                    prinkCountry(parseCsvLine(line));
+
+            while ((line = br.readLine()) != null) {
+                printCountry(parseCsvLine(line));
             }
-            System.out.println("done");
+
         } catch (IOException e) {
-            System.out.println("Wrong");
-        }
-        finally {
+            e.printStackTrace();
+        } finally {
             try {
-                if(br!=null){
+                if (br != null)
                     br.close();
-                }
-            }catch (IOException E){
-                E.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        System.out.println(file.getAbsolutePath());
     }
-    public  static List<String> parseCsvLine(String csvLine){
+
+    public static List<String> parseCsvLine(String csvLine) {
         List<String> result = new ArrayList<>();
-        if(csvLine!=null){
+        if (csvLine != null) {
             String[] splitData = csvLine.split(",");
-            for(int i =0;i<splitData.length;i++){
+            for (int i = 0; i < splitData.length; i++) {
                 result.add(splitData[i]);
             }
         }
         return result;
     }
-    public  static  void prinkCountry(List<String > country){
-        System.out.println( "Country [id= "
-                + country.get(0)
-                + ", code= " + country.get(1)
-                + " , name=" + country.get(2)
-                + "]");
+
+    private static void printCountry(List<String> country) {
+        System.out.println(
+                "Country [id= "
+                        + country.get(0)
+                        + ", code= " + country.get(1)
+                        + " , name=" + country.get(2)
+                        + "]");
+    }
     }
 
-}
